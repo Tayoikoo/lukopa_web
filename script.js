0192a063c611b8458f72de191207a4f1d17d0b80
Tayoikoo <luthfifalseman@gmail.com>
2023-05-11 11:50:47 +0700

    🥺

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-isDark = true;
-var btn = document.getElementById("btn-toggle");
-// btn.innerHTML= '<img src="https://http.cat/404" style="max-width:100px; max-height:100px;">';
-btn.innerHTML= 'Dark Mode: OFF';
-btn.addEventListener("click", function(){
-  var body = document.body;
-
-  if (!isDark) {
-    // Dark
-    body.classList.remove("bg-white");
-    body.classList.add("bg-dark");
-    isDark = true;
-    btn.innerHTML= 'Dark Mode: ON';
-    btn.classList.remove("btn-white");
-    btn.classList.add("btn-dark");
-  } else {
-    // White
-    body.classList.remove("bg-dark");
-    isDark = false;
-    btn.innerHTML= 'Dark Mode: OFF';
-    btn.classList.remove("btn-dark");
-    btn.classList.add("btn-white");
-  }
-});
-
 //time
 var tampilWaktu = document.getElementById("time");
 
 function waktu(){
   var stringWaktu = new Date().toLocaleTimeString("id-ID",{timeZone: "Asia/Jakarta"});
-  var formatString = stringWaktu.replace("," , "-");
+  var formatString = stringWaktu.replace("." , ":");
   tampilWaktu.innerHTML = formatString;
 }
 
